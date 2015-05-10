8c5da444d683046b3a2b98fde24583aa3076557c
raymondbh <raymondbh@gmail.com>
2015-05-10 17:25:33 +0200

    Server anvil recipes superseed client recipes. (TFC-0.79.18+)

--- src/java/sladki/tfc/Handlers/ChunkEventHandler.java
+++ src/java/sladki/tfc/Handlers/ChunkEventHandler.java
@@ -1,5 +1,6 @@
 package sladki.tfc.Handlers;
 
+import com.bioxx.tfc.Core.Recipes;
 import net.minecraftforge.event.world.WorldEvent;
 import sladki.tfc.ModManager;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -8,7 +9,7 @@ public class ChunkEventHandler {
 
 	@SubscribeEvent
 	public void onLoadWorld(WorldEvent.Load event) {
-		if(event.world.provider.dimensionId == 0) {
+		if(!event.world.isRemote && event.world.provider.dimensionId == 0 ) {
 			ModManager.registerAnvilRecipes();
 		}
 	}
